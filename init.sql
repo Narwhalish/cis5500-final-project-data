6e789005c9e8c51624f816595a355227f44645c9
Bryan Yao <dev@bryanyao.com>
2023-04-26 16:39:53 -0400

    Remove extra DDL code.

--- init.sql
+++ init.sql
@@ -53,26 +53,4 @@ CREATE TABLE IF NOT EXISTS locations (
     slug VARCHAR(255),
     website VARCHAR(255)
 );
-CREATE TABLE IF NOT EXISTS locations1 (
-    sid BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id INT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    street VARCHAR(255),
-    zip VARCHAR(255),
-    city VARCHAR(255),
-    region VARCHAR(255),
-    cd VARCHAR(8),
-    phone VARCHAR(255),
-    blurb VARCHAR(255),
-    dir_city_id VARCHAR(255),
-    dir_city_name VARCHAR(255),
-    dir_city_slug VARCHAR(255),
-    dir_country_id VARCHAR(255),
-    dir_country_name VARCHAR(255),
-    lat FLOAT NOT NULL,
-    lng FLOAT NOT NULL,
-    primary_alias_on_fb VARCHAR(255),
-    slug VARCHAR(255),
-    website VARCHAR(255)
-);
 
